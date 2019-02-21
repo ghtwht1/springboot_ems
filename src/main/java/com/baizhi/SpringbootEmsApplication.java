fb64a69154a509aea0b36e74bfcbbc7e90879b31
ghtwht1 <39364108+ghtwht1@users.noreply.github.com>
2019-02-21 20:01:25 +0800

    Update SpringbootEmsApplication.java

--- src/main/java/com/baizhi/SpringbootEmsApplication.java
+++ src/main/java/com/baizhi/SpringbootEmsApplication.java
@@ -22,6 +22,7 @@ public class SpringbootEmsApplication {
     public HttpMessageConverters fastJsonHttpMessageConverters() {
         // 1. 需要定义一个converter转换消息的对象
         FastJsonHttpMessageConverter fasHttpMessageConverter=new FastJsonHttpMessageConverter();
+        
         // 2. 添加fastjson的配置信息，比如:是否需要格式化返回的json的数据
         FastJsonConfig fastJsonConfig=new FastJsonConfig();
         fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
