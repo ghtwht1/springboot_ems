128b3b27e329326c1f2532fc2f52d0f5cea556da
ljt <1010437140@qq.com>
2019-02-22 15:48:50 +0800

    v2.0

--- src/main/java/com/baizhi/controller/test.java
+++ src/main/java/com/baizhi/controller/test.java
@@ -2,6 +2,6 @@ package com.baizhi.controller;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello zhangsan");
     }
 }
